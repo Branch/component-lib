0ecc83ffa2082f030bda2de9c28476b39a4d8a09
Andreas Beuger <andreasbeuger@gmail.com>
2021-12-21 15:19:47 +0100

    Updated .bitmap, removed circle tests

--- neumorphism/ui/circle/circle.spec.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicCircle } from './circle.composition';
-
-it('should render with the correct styles and width', () => {
-  const { getByText } = render(<BasicCircle />);
-  const rendered = getByText('hello from Circle');
-  expect(rendered).toBeTruthy();
-});
