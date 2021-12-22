e26680526179909f409301979932fe1f51b7699e
Andreas Beuger <andreasbeuger@gmail.com>
2021-12-22 23:54:25 +0100

    slider component

---
+++ neumorphism/ui/slider/slider.composition.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Slider } from './slider';
+
+export const BasicSlider = () => (
+  <Slider type="convex" padding={[20, 10, 20, 10]} size={""} speed={35} images={
+      ['https://cdn.worldvectorlogo.com/logos/typescript.svg', 'https://cdn.worldvectorlogo.com/logos/typescript.svg', 'https://cdn.worldvectorlogo.com/logos/typescript.svg', 'https://cdn.worldvectorlogo.com/logos/typescript.svg', 'https://cdn.worldvectorlogo.com/logos/typescript.svg', 'https://cdn.worldvectorlogo.com/logos/typescript.svg', 'https://cdn.worldvectorlogo.com/logos/typescript.svg', 'https://cdn.worldvectorlogo.com/logos/typescript.svg']
+  } />
+);
---
+++ neumorphism/ui/slider/slider.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+
+import classNames from 'classnames/bind';
+
+import { neuStyles } from '@branch/neumorphism.styles.neustyles';
+import slideStyles from './slider.module.scss';
+
+export type SliderProps = {
+  /**
+   * Speed of slider in seconds
+   */
+  speed: number
+
+  /**
+   * Array of image sources
+   */
+  images: string[]
+
+  /**
+   * type of card
+   */
+  type?: string;
+
+  /**
+   * Border radius size
+   */
+  size?: string
+
+  /**
+   * Padding
+   */
+  padding?: number[];
+};
+
+export function Slider({speed, images, type, size, padding }: SliderProps) {
+
+  const cx = classNames.bind(neuStyles);
+  const className = cx({
+    neuelement: true,
+    flat: type === 'flat',
+    convex: type === 'convex',
+    concave: type === 'concave',
+    pressed: type === 'pressed',
+    sm: size === 'sm',
+    md: size === 'md',
+    lg: size === 'lg'
+  });
+
+  const slideBinding = classNames.bind(slideStyles);
+  const slideTrackType = slideBinding({
+    track: true
+  })
+
+  const trackInlineStyles = {
+    width: 250 * images.length,
+    animation: "scroll " + speed + "s linear infinite"
+  }
+
+  const sliderInlineStyles = {
+    padding: padding.join("px ") + "px"
+}
+
+  return (
+    <div className={cx(className, slideStyles.slider)} style={sliderInlineStyles}>
+      <div className={slideTrackType} style={trackInlineStyles}>
+        {images.map(image => {
+          return (
+              <div className={slideStyles.slide}>
+                <img src={image} alt="logo" />
+              </div>
+          )
+        })
+        }
+      </div>
+
+      <style>{`
+          @keyframes scroll {
+            0% {
+              transform: translateX(calc(0));
+            }
+            100% {
+              transform: translateX(calc(-250px * ${images.length / 2}));
+            }
+          }
+    `}</style>
+    </div>
+  );
+}
