db0760e870b2b995b69707e0fc84c704c5069c1d
Andreas Beuger <andreasbeuger@gmail.com>
2021-12-27 18:09:47 +0100

    Link component

---
+++ neumorphism/ui/link/link.composition.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from './link';
+
+export const BasicLink = () => (
+    <Link text="hello from Link" url="https://bit.dev" type="flat" padding={[10, 20, 10, 20]} size={""} newWindow={true} />
+);
---
+++ neumorphism/ui/link/link.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import classNames from "classnames/bind";
+import { neuStyles } from '@branch/neumorphism.styles.neustyles';
+import linkStyles from './link.module.scss';
+
+export type LinkProps = {
+  /**
+   * a text to be rendered in the component.
+   */
+  text: string
+  /**
+   * type of link
+   */
+  type?: string;
+
+  /**
+   * Border radius size
+   */
+  size?: string
+
+  /**
+   * Padding
+   */
+  padding?: number[];
+
+  /**
+   * Url
+   */
+  url: string
+
+  /**
+   * Open in new tab
+   */
+  newWindow?: boolean
+};
+
+export function Link({ text, type, size, padding, url, newWindow }: LinkProps) {
+
+  const inlineStyles = {
+    padding: padding.join("px ") + "px"
+  }
+
+  const cx = classNames.bind(neuStyles);
+  const className = cx({
+    neuelement: true,
+    hoverable: true,
+    flat: type === 'flat',
+    convex: type === 'convex',
+    concave: type === 'concave',
+    pressed: type === 'pressed',
+    sm: size === 'sm',
+    md: size === 'md',
+    lg: size === 'lg'
+  });
+
+  return (
+    <a className={classNames(linkStyles, className)} href={url} style={inlineStyles} target={newWindow === true ? '_blank' : '_self'}>
+      {text}
+    </a>
+  );
+}
